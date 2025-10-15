6775f1d7f875aaa9df25451b023cc117f86e0990
Eshwar <59696182+eshwar75@users.noreply.github.com>
2025-10-16 02:18:55 +0530

    feat: added one jest package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/setupTests.ts',
+		exclude: [...configDefaults.exclude, '**/packages/template/*'],
+	},
 });
